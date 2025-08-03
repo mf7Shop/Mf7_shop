2b5b152fd359d9c24674f9c4274f0845fc5179d9
ELEVEN <adamabarkach2@gmail.com>
2025-08-03 17:02:36 +0100

    blue

--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -168,7 +168,7 @@ const AddProduct = () => {
             />
           </div>
         </div>
-        <button type="submit" className="px-8 py-2.5 bg-orange-600 text-white font-medium rounded">
+        <button type="submit" className="px-8 py-2.5 bg-blue-600 text-white font-medium rounded">
           ADD
         </button>
       </form>
--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -144,7 +144,7 @@ const OrderSummary = () => {
               placeholder="Enter promo code"
               className="flex-grow w-full outline-none p-2.5 text-gray-600 border"
             />
-            <button className="bg-orange-600 text-white px-9 py-2 hover:bg-orange-700">
+            <button className="bg-blue-600 text-white px-9 py-2 hover:bg-blue-700">
               Apply
             </button>
           </div>
@@ -172,7 +172,7 @@ const OrderSummary = () => {
         </div>
       </div>
 
-      <button onClick={createOrder} className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700">
+      <button onClick={createOrder} className="w-full bg-blue-600 text-white py-3 mt-5 hover:bg-blue-700">
         Place Order
       </button>
     </div>
